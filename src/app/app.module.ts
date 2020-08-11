e84ea855329f7f856a36c5ea547b92fc66c633fe
Yana Harris <yanaha@cisco.com>
2020-08-11 15:17:28 -0700

    cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import {ForecastComponent } from './components//forecast.component';
+import { ForecastComponent } from './components//forecast.component';
 
 @NgModule({
   declarations: [
